102c79ef931f3a3bce053cba2c062c12d5c1a05f
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-03 20:11:34 +0300

    added a save function

--- run.py
+++ run.py
@@ -7,4 +7,19 @@ print("welcome to password manager,\n\tplease login..")
 
 class credentials:
     '''
+    These class accepts account_name,username, password.
+    It stores them as a dictionary.
+
     '''
+    def __init__(self,account,username,password):
+
+        self.account = account
+        self.username = username
+        self.password = password
+    
+    def save(self):
+        f=open("account.text","a")
+        f.write(self.account+ "\n")
+        f.write(self.username + "\n")
+        f.write(self.password + "\n")
+        f.close()
