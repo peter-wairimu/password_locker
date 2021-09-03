0f7381e8b0a14a83153358b6567e40784a947c98
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-03 14:35:08 +0300

    modified the password page

--- locker.py
+++ locker.py
@@ -1,7 +1,9 @@
- class Password:
+class Password:
     """
     Class that generates a users emails
     """
+
+    contact_list =[]
 
     def __init__(self,first_name,last_name,email):
         self.first_name = first_name
