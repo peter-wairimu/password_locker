e4e1ddca6d941694f141b88a05141387b2dff8bd
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-03 16:42:09 +0300

    added run.py file

--- locker_test.py
+++ locker_test.py
@@ -7,4 +7,26 @@ class TestPassword(unittest.TestCase):
 
     '''
     def setUp(self):
-        return 
+        '''
+        Set up method to run before each test cases
+
+        '''
+        self.new_contact = Password("peter","chege","peterchege@gmail.com")
+
+    def test_init(self):
+            '''
+            test_init test case to test if the object is initialized properly
+
+            '''
+            self.assertEqual(self.new_contact.first_name,"peter")
+            self.assertEqual(self.new_contact.last_name,"chege")
+            self.assertEqual(self.new_contact.email,"peterchege@gmail.com")
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
