b738152bbb96778a67bbbd33401e17ee204b10e2
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-03 16:10:14 +0300

    added a class testpassword

--- locker_test.py
+++ locker_test.py
@@ -1,2 +1,10 @@
 import unittest
 from locker import Password
+
+class TestPassword(unittest.TestCase):
+    '''
+    Test class that defines test cases for the password class behaviour
+
+    '''
+    def setUp(self):
+        return 
